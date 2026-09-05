560301c1a95290ac43d68ebd765dd11c72a8d51d
Atharva <finitecode.contact@gmail.com>
2026-09-05 21:35:45 +0530

    3AM: Theme: Remove obsolete SDK check, since targets are always gonna be higher

--- app/src/main/java/com/slate/music/ui/theme/Theme.kt
+++ app/src/main/java/com/slate/music/ui/theme/Theme.kt
@@ -44,7 +44,7 @@ fun MusicTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor && true -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
